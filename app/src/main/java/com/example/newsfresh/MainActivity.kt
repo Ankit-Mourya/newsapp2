0cb2b1b35abfd4527a9e7adda4565a8de518c3e8
shubhasai <mshubhasai2002@gmail.com>
2023-09-29 21:56:14 +0530

    Error Log Added

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -8,8 +8,10 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
     private lateinit var mAdapter: NewsListAdapter
@@ -28,30 +30,51 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
           val url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ff4bbaf7879c4e648465b17b432574ef"
 
 //        https://newsapi.org/v2/everything?q=tesla&from=2023-07-05&sortBy=publishedAt&apiKey=ff4bbaf7879c4e648465b17b432574ef
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, {
-              val newsJsonArray = it.getJSONArray("articles")
-              val newsArray = ArrayList<News>()
-            Log.d("Api Call","2")
-            for (i in 0 until newsJsonArray.length()){
-                Log.d("Api Call","3")
-                val newsJsonObject = newsJsonArray.getJSONObject(i)
-                Log.d("akm", "start ")
-                val news = News(
-                   newsJsonObject.getString("title"),
-                   newsJsonObject.getString("author"),
-                    newsJsonObject.getString("url"),
-                    newsJsonObject.getString("urlToImage")
-                )
-                Log.d("Api Call","4")
-                newsArray.add(news)
-            }
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+                try {
+                    val newsJsonArray = response.getJSONArray("articles")
+                    val newsArray = ArrayList<News>()
+                    for (i in 0 until newsJsonArray.length()) {
+                        val newsJsonObject = newsJsonArray.getJSONObject(i)
+                        val news = News(
+                            newsJsonObject.getString("title"),
+                            newsJsonObject.getString("author"),
+                            newsJsonObject.getString("url"),
+                            newsJsonObject.getString("urlToImage")
+                        )
+                        newsArray.add(news)
+                    }
+                    mAdapter.updateNews(newsArray)
+                } catch (e: JSONException) {
+                    // Handle JSON parsing error
+                    Log.e("Api Call", "JSON parsing error: ${e.message}")
+                }
+            },
+            { error ->
+                // Handle Volley error
+                Log.e("Api Call", "Volley error: ${error.message}")
+                if (error is VolleyError) {
+                    if (error.networkResponse != null) {
+                        val statusCode = error.networkResponse.statusCode
+                        val responseData = String(error.networkResponse.data)
+                        val responseHeaders = error.networkResponse.headers
+                        Log.e("Api Call", "Volley network error - Status code: $statusCode")
+                        Log.e("Api Call", "Response headers: $responseHeaders")
+                        Log.e("Api Call", "Response data: $responseData")
+                    } else {
+                        Log.e("Api Call", "Volley error: ${error.message}")
+                    }
+                } else {
+                    Log.e("Api Call", "Unknown error")
+                }
 
-               mAdapter.updateNews(newsArray)
-            Log.d(  "times","succeed" )
-        },
-            {
-              }
+            }
         )
+
+// Access the RequestQueue through your singleton class
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+
 
 // Access the RequestQueue through your singleton class.
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
